44521a702d1c3a534ef5e73998efdd3243428848
MoonWalker <silavsale@gmail.com>
2023-03-13 18:54:54 -0400

    update ci/cd

--- test/App.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { render } from "@testing-library/react"
-import App from "../src/App"
-
-test("renders App with title", () => {
-  const { getByText } = render(<App />)
-
-  expect(getByText(/ENV MODE/i)).toBeInTheDocument()
-})
